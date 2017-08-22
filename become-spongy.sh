821553d40de9a77965dc3e1f3a967bb1d1c91686
Roberto Tyley <roberto.tyley@gmail.com>
2017-08-22 19:43:03 +0100

    Map the `test` project as well

--- become-spongy.sh
+++ become-spongy.sh
@@ -4,9 +4,9 @@
     
 find -name bouncycastle | xargs rename s/bouncycastle/spongycastle/
 
-find {core,jce,kmip,misc,pg,pkix,prov,tls} -type f | xargs -I '{}' ssed -i -R 's/bouncycastle(?!.org)/spongycastle/g' '{}'
+find {core,jce,kmip,misc,pg,pkix,prov,tls,test} -type f | xargs -I '{}' ssed -i -R 's/bouncycastle(?!.org)/spongycastle/g' '{}'
 
 # BC to SC for provider name
     
-find {core,jce,kmip,misc,pg,pkix,prov,tls} -type f | xargs -I '{}' sed -i s/\"BC\"/\"SC\"/g '{}'
+find {core,jce,kmip,misc,pg,pkix,prov,tls,test} -type f | xargs -I '{}' sed -i s/\"BC\"/\"SC\"/g '{}'
 
