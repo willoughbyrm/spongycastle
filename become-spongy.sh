4935de002481ac4bc19416f9cd10bb8887e72928
Roberto Tyley <roberto.tyley@gmail.com>
2017-08-28 11:21:54 +0100

    bash is being awful

--- become-spongy.sh
+++ become-spongy.sh
@@ -2,11 +2,12 @@
 
 # Package rename org.bouncycastle to org.spongycastle
     
-find -name bouncycastle | xargs rename s/bouncycastle/spongycastle/
+# find -name bouncycastle | xargs rename s/bouncycastle/spongycastle/
 
-find {core,jce,kmip,misc,pg,pkix,prov,tls,test} -type f | xargs -I '{}' ssed -i -R 's/bouncycastle(?!.org)/spongycastle/g' '{}'
+# find {core,jce,kmip,misc,pg,pkix,prov,tls,test} -type f | xargs -I '{}' ssed -i -R 's/bouncycastle(?!.org)/spongycastle/g' '{}'
 
 # BC to SC for provider name
     
-find {core,jce,kmip,misc,pg,pkix,prov,tls,test} -type f | xargs -I '{}' sed -i s/\"BC\"/\"SC\"/g '{}'
+find {core,jce,kmip,misc,pg,pkix,prov,tls,test} -type f | xargs -I '{}' ssed -i -R s/'"'BC(|JSSE)?'"'/'"'SC$1'"'/g '{}'
+
 
